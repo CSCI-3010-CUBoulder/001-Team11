14ce0d903c561099c7ddb48089b6f6c0e2ea010a
Greg <greglenard@gmail.com>
2020-02-10 17:40:18 -0700

    functions test

--- tests.cpp
+++ tests.cpp
@@ -1,2 +1,7 @@
 #define CATCH_CONFIG_MAIN
-include "catch.hpp"
+#include "catch.hpp"
+
+TEST_CASE ("Factorial test cases:", ["factorials"]) {
+    REQUIRE( Factorial(2) == 2);
+    REQUIRE( Factorial(3) == 6);
+}
